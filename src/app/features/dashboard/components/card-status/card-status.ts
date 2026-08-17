4de2afd4b205e63a05db9965e664a893795c1820
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2026-08-17 20:50:20 -0300

    refactor: added cards status to the dashboard projetcs page

--- src/app/features/dashboard/components/card-status/card-status.ts
+++ src/app/features/dashboard/components/card-status/card-status.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from "@angular/material/icon";
